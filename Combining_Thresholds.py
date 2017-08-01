e8efb06620e6c37915ebe0c61073cfa7d19ee823
Guo Ruichen <guorch@buaa.edu.cn>
2017-08-01 22:03:45 +0800

    direction threshold

--- Combining_Thresholds.py
+++ Combining_Thresholds.py
@@ -1,4 +1,13 @@
+import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pickle
+
+
+# Read in an image
+image = mpimg.imread('signs_vehicles_xygrad.png')
+
 
 def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
     # Calculate directional gradient
@@ -13,7 +22,25 @@ def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
 def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
     # Calculate gradient direction
     # Apply threshold
-    return dir_binary
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    # 2) Take the gradient in x and y separately
+    # 3) Take the absolute value of the x and y gradients
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
+    # 5) Create a binary mask where direction thresholds are met
+    # 6) Return this mask as your binary_output image
+    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+    dir_gradient = np.arctan2(abs_sobely, abs_sobelx)
+    binary = np.zeros_like(dir_gradient)
+    min = thresh[0]
+    max = thresh[1]
+    binary[(dir_gradient >=  min) & (dir_gradient <= max)] = 1
+
+    return binary
 
 # Choose a Sobel kernel size
 ksize = 3 # Choose a larger odd number to smooth gradient measurements
@@ -23,3 +50,6 @@ gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
 grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
 mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
 dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+
+combined = np.zeros_like(dir_binary)
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
